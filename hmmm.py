8cc4ff9b78b140997c1d52b543da61055bf48a95
arkarifqi5-cmd <arkarifqi5@gmail.com>
2025-11-01 23:22:35 +0700

    Add files via upload

---
+++ hmmm.py
@@ -0,0 +1,16 @@
+nama = input("masukan nama lu/kamu :")
+umur = int(input("masukan umur lu/kamu :"))
+
+if umur == 19 :
+    print("hbd",nama,"semoga di umur lu/kamu sekarang,semoga mimpi lu/kamu di tahun kemaren belum terwujud.")
+    print("semoga di umur/tahun sekarang bisa terwujud,ok segitu aja,semoga projeck yang saya buat ini berhasil.")
+    print("harusnyosih,kalo dak berhasil berarti pesan ini tidak tersampaikan.")
+    print("soalnyo baru DAY 5 belajar coding.")
+    print("gws,semoga cepat sembuh.")
+    print("BY:hamba allah")
+
+
+else:
+    print("lohh,bukannyo umur lu/kamu 19 yo.")
+    print("apa lupa karna ketuaan,wkwwk.")
+    print("cobak di ulang,buat yang benar.")
